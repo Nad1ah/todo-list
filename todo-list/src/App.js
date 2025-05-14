adc7242bc71188a228e8cb7ff2ae02bb07ceeadd
Nadiah Mauricio <nadiahmauricio@gmail.com>
2025-05-14 15:24:21 +0100

    try 2

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,6 +2,20 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TaskList from "./TaskList";
 import AddTask from "./AddTask";
+import { API_URL } from "./config";
+
+async function fetchTasks() {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const tasks = await response.json();
+    return tasks;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
